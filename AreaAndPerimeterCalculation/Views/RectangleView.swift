bb339133560e049c46a84510a364e3b5b2f7608c
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-17 11:38:55 -0500

    set up an empty view to calculate the area of a rectangle

---
+++ AreaAndPerimeterCalculation/Views/RectangleView.swift
@@ -0,0 +1,20 @@
+//
+//  RectangleView.swift
+//  AreaAndPerimeterCalculation
+//
+//  Created by Evelyn Chen on 2023-01-17.
+//
+
+import SwiftUI
+
+struct RectangleView: View {
+    var body: some View {
+        
+    }
+}
+
+struct RectangleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleView()
+    }
+}
