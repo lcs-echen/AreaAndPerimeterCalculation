e7b26f42afcda89963bb427c1b47193ced5b5e64
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-17 11:34:48 -0500

    Initial Commit

---
+++ AreaAndPerimeterCalculation/AreaAndPerimeterCalculationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaAndPerimeterCalculationApp.swift
+//  AreaAndPerimeterCalculation
+//
+//  Created by Evelyn Chen on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct AreaAndPerimeterCalculationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
